d0f133cc18003991ad5e35aebfb96a200d6373dc
huzandevil <huzandevil@gmail.com>
2016-07-26 11:30:22 +0800

    1

---
+++ app/src/main/java/com/our/coolgroup/artist/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.our.coolgroup.artist;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ZAN on 2016/7/26.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+
+
+}
---
+++ app/src/main/java/com/our/coolgroup/artist/MainActivity.java
@@ -0,0 +1,12 @@
+package com.our.coolgroup.artist;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
